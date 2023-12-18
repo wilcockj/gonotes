dd6237de93a23495e6c72984ebf0dcfe009183fa
James Wilcock <jameswilcock8@gmail.com>
2023-12-17 21:06:58 -0800

    sqlite simple

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wilcockj/gonotes
 
 go 1.21.4
+
+require github.com/mattn/go-sqlite3 v1.14.19
--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
 	"net/http"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello\n")
+	fmt.Fprintf(w, "uhh yeah\n")
 }
 
 func main() {
+	db, err := sql.Open("sqlite3", "./notes.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sqlStmt := `
+	create table foo (id integer not null primary key, name text);
+	delete from foo;
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
 	http.HandleFunc("/hello", hello)
 	fmt.Println("Beggining serving")
 	http.ListenAndServe(":8090", nil)

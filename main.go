47366c0e188d28a98865f5dae642f381e801db69
James Wilcock <jameswilcock8@gmail.com>
2023-12-19 13:39:39 -0800

    basic sqlite3 working

--- main.go
+++ main.go
@@ -6,21 +6,37 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
+	"os"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "uhh yeah\n")
 }
 
+/*
+* want to creat note taking app backend
+* allow user to add note
+* fetch/view note, edit note maybe only send diff?
+ */
 func main() {
+	os.Remove("./notes.db")
 	db, err := sql.Open("sqlite3", "./notes.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	sqlStmt := `
-	create table foo (id integer not null primary key, name text);
+	create table foo (id integer not null primary key, name text, notes text);
 	delete from foo;
 	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+	sqlStmt = `
+  insert into foo
+  values (1,"note name","notes notes my notes are really notes");
+  `
 	_, err = db.Exec(sqlStmt)
 	if err != nil {
 		log.Printf("%q: %s\n", err, sqlStmt)

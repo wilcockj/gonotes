84c6a1f49daf87d4d3e401ae38f260b8d051da63
James Wilcock <jameswilcock8@gmail.com>
2023-12-20 14:27:51 -0800

    got notes title and body posting to server

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
-	"html/template"
 	"io"
 	"log"
 	"net/http"
 	"os"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
+func home(w http.ResponseWriter, req *http.Request) {
 	file, err := os.Open("templates/index.html")
 	if err != nil {
 		log.Fatal(err)
@@ -29,7 +28,8 @@ func hello(w http.ResponseWriter, req *http.Request) {
 
 func addnotes(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	fmt.Printf("got notes val %s\n", r.FormValue("notetitle"))
+	fmt.Printf("got notes title %s\n", r.FormValue("notetitle"))
+	fmt.Printf("got notes body %s\n", r.FormValue("notebody"))
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 
 }
@@ -68,52 +68,8 @@ func main() {
 		log.Printf("%q: %s\n", err, sqlStmt)
 		return
 	}
-	const tpl = `
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>{{.Title}}</title>
-	</head>
-	<body>
-		{{range .Items}}<div>{{ . }}</div>{{else}}<div><strong>no rows</strong></div>{{end}}
-	</body>
-</html>
-`
 
-	check := func(err error) {
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	t, err := template.New("webpage").Parse(tpl)
-	check(err)
-	data := struct {
-		Title string
-		Items []string
-	}{
-		Title: "My page",
-		Items: []string{
-			"My photos",
-			"My blog",
-		},
-	}
-
-	err = t.Execute(os.Stdout, data)
-	check(err)
-
-	noItems := struct {
-		Title string
-		Items []string
-	}{
-		Title: "My another page",
-		Items: []string{},
-	}
-
-	err = t.Execute(os.Stdout, noItems)
-	check(err)
-
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", home)
 	http.HandleFunc("/notes", addnotes)
 	fmt.Println("Beggining serving on port 9060")
 	http.ListenAndServe(":9060", nil)
